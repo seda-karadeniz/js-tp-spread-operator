0ac9bea548e63907dd8c94593e806f3a7c3622ff
Daniel Schreurs <daniedaniel.schreurs@hotmail.com>
2019-11-27 17:40:33 +0100

    :tada: Création solution

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+const SpreadOperator = {
+  cacheDom() {
+    this.heading = document.querySelector('.jump');
+    this.aLetters = Array.from(this.heading.textContent);
+  },
+  init() {
+    window.document.documentElement.classList.add('js-enabled');
+    this.cacheDom();
+    this.removeRemoveElements(this.heading);
+    for (const letter of this.aLetters) {
+      const eSpan = document.createElement('span');
+      eSpan.appendChild(document.createTextNode(letter === ' ' ? '\u00A0' : letter));
+      this.heading.appendChild(eSpan);
+    }
+  },
+  removeRemoveElements(node) {
+    while (node.firstChild) {
+      node.removeChild(node.firstChild);
+    }
+  }
+};
+
+SpreadOperator.init();
